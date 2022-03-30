8a7eaa3f54033eb17d537ac41e11bccc62618102
lekejosh <lekejosh6wf@gmail.com>
2022-03-30 18:13:18 +0100

    Menu toggle, dark/light theme toggle

---
+++ asset/js/main.js
@@ -0,0 +1,50 @@
+// Grab elements
+const selectElement = (selector) => {
+  const element = document.querySelector(selector);
+  if (element) return element;
+  throw new Error(
+    `Something went wrong, make sure that the ${selector} exists or typed correctly.`
+  );
+};
+
+// Nav Styles on scroll
+const scrollHeader = () => {
+  const headerElement = selectElement("#header");
+  if (this.scrollY >= 15) {
+    headerElement.classList.add("activated");
+  } else {
+    headerElement.classList.remove("activated");
+  }
+};
+
+window.addEventListener("scroll", scrollHeader);
+
+//Open & search pop-up
+const menuToggleIcon = selectElement("#menu-toggle-icon");
+
+const toggleMenu = () => {
+  const mobileMenu = selectElement("#menu");
+  mobileMenu.classList.toggle("activated");
+  menuToggleIcon.classList.toggle("activated");
+};
+
+menuToggleIcon.addEventListener("click", toggleMenu);
+
+// Switch theme and add to local storage
+const bodyElement = document.body;
+const themeToggleBtn = selectElement("#theme-toggle-btn");
+const currentTheme = localStorage.getItem("currentTheme");
+
+if (currentTheme) {
+  bodyElement.classList.add("light-theme");
+}
+
+themeToggleBtn.addEventListener("click", () => {
+  bodyElement.classList.toggle("light-theme");
+
+  if (bodyElement.classList.contains("light-theme")) {
+    localStorage.setItem("currentTheme", "themeActive");
+  } else {
+    localStorage.removeItem("currentTheme");
+  }
+});
