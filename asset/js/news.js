79bb7ef5e217380ccd7bf1b4c920d624a4d54389
lekejosh <lekejosh6wf@gmail.com>
2022-04-03 20:06:45 +0100

    Arrays and Apis

--- asset/js/news.js
+++ asset/js/news.js
@@ -8,4 +8,29 @@ const weatherBtn = document.getElementById("weather");
 
 const searchBtn = document.getElementById("search-icon");
 
+const newsQuery = document.getElementById("newsQuery");
+const newsType = document.getElementById("newsType");
+
+//Array
+var newsDataArr = [];
+
 //Apis
+const API_KEY = "b6a81689d89a4cd2bb745925b71f71f5";
+const HEADLINE_NEWS = "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
+const SPORT_NEWS =
+  "https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=";
+const BUSINESS_NEWS =
+  "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=";
+const TECHNOLOGY_NEWS =
+  "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=";
+const SEARCH_NEWS = "https://newsapi.org/v2/everything?q=";
+
+generalBtn.addEventListener("click", function () {});
+
+sportsBtn.addEventListener("click", function () {});
+
+stocksBtn.addEventListener("click", function () {});
+
+cryptoBtn.addEventListener("click", function () {});
+
+weatherBtn.addEventListener("click", function () {});
