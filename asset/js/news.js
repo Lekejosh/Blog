310b2d43c2e3d37b777d1733068ef71654da680e
lekejosh <lekejosh6wf@gmail.com>
2022-04-03 19:36:20 +0100

    Variables for Api

---
+++ asset/js/news.js
@@ -0,0 +1,11 @@
+//Variables
+
+const generalBtn = document.getElementById("general");
+const sportsBtn = document.getElementById("sports");
+const stocksBtn = document.getElementById("stocks");
+const cryptoBtn = document.getElementById("crypto");
+const weatherBtn = document.getElementById("weather");
+
+const searchBtn = document.getElementById("search-icon");
+
+//Apis
